79328eab9526096330d9bf7056cde811bb4d7c26
jglrxavpok <jglrxavpok@gmail.com>
2017-06-14 21:48:03 +0200

    Ajout de deux tests pour vérifier que l'implémentation de la mémoire de mémoïsation fonctionne

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Ce projet vise la création d'un langage de programmation utilisable pour du des
 
 Dépendances
 ===========
-* [ANTLR](http://antlr.org): Fournit un lexeur et un parseur se basant sur une grammaire à définir soi-même
+* [ANTLR](http://antlr.org) (ANother Tool for Language Recognition): Fournit un lexeur et un parseur se basant sur une grammaire à définir soi-même
 * [AbstractTypeInference](https://github.com/jglrxavpok/AbstractTypeInference): Permet de faire de l'inférence de types à partir d'un arbre représentant l'expression dont on cherche à inférer le type
 * [ASM](http://asm.ow2.org/): Manipulation et génération de bytecode Java (pour la Java Virtual Machine)
---
+++ src/test/kotlin/TestMemoizationCache.kt
@@ -0,0 +1,26 @@
+import euclin.intrisincs.MemoizationCache
+import org.junit.Test
+
+class TestMemoizationCache {
+
+    @Test
+    fun testSingleArgument() {
+        val cache = MemoizationCache(1)
+        cache.set(arrayOf("Testabc"), "Mon texte")
+
+        assert(cache.has(arrayOf("Testabc"))) { "Le cache ne connait pas la clé donnée" }
+        val cached = cache.get(arrayOf("Testabc"))
+        assert(cached == "Mon texte") { "Le cache n'a pas la bonne valeur, la valeur est: $cached" }
+    }
+
+    @Test
+    fun testMultipleArguments() {
+        val cache = MemoizationCache(5)
+        val args = arrayOf("Dzd", 45, Unit, TestMemoizationCache::class, this::testMultipleArguments)
+        cache.set(args, "Mon texte")
+
+        assert(cache.has(args)) { "Le cache ne connait pas la clé donnée" }
+        val cached = cache.get(args)
+        assert(cached == "Mon texte") { "Le cache n'a pas la bonne valeur, la valeur est: $cached" }
+    }
+}
